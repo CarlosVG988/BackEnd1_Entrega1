8ac6dab2e50841a771efa5329ba758be7ff7d85c
Carlos Valderrama <carlosvg.988@gmail.com>
2024-10-05 17:24:56 -0300

    Version base

---
+++ src/services/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cartsRoutes from "../routes/carts.routes.js"
+import productsRoutes from "../routes/products.routes.js"
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use("/api/products",productsRoutes)
+app.use("/api/carts",cartsRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Servidor escuchando en puerto ${PORT}`)
+})
